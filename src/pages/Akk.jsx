bd4f6c0ad0aa25718771b235d8cf8898ca94787e
kfsaidamir <skadirov2009@gmail.com>
2025-03-27 15:35:30 +0500

    a

---
+++ src/pages/Akk.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Join from '../components/Join/Join'
+
+const Akk = () => {
+  return (
+  <>
+  <Header/>
+ <Join/>
+  </>
+  )
+}
+
+export default Akk
